8451ea2dcf4b083d6d441327288d2f998de064aa
MarianaSavoldi <marianasavodi123@gmail.com>
2024-05-01 16:56:00 -0300

    feat(sign-up): create registerRestaurant request function

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+interface RegisterRestaurantBody {
+  restaurantName: string
+  managerName: string
+  email: string
+  phone: string
+}
+
+export const registerRestaurant = async ({
+  restaurantName,
+  managerName,
+  email,
+  phone,
+}: RegisterRestaurantBody) => {
+  await api.post('/restaurants', { restaurantName, managerName, email, phone })
+}
